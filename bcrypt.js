7e5c2f4769e4994b8db7eee8caa4244ea926152f
NgWJ422 <jng010422@gmail.com>
2023-04-12 06:42:06 +0800

    adding normal ver

---
+++ bcrypt.js
@@ -0,0 +1,66 @@
+const bcrypt = require('bcrypt');
+const express = require('express')
+const app = express()
+const port = 3000
+
+let dbuser =[
+]
+app.use(express.json())
+
+
+app.post('/', (req, res) => {
+  let data= req.body
+  let loginresult = login(
+    data.username,
+    data.password
+  )
+  res.send({
+    status: loginresult,
+    originaldata: data,
+    date: Date.now
+  });
+});
+
+app.post('/login',async(req,res) =>{
+  const{username, password}=req.body;
+
+  const user = dbuser.find(user => user.username === username);
+
+  if(!user){
+    res.send({error:"user not found"});
+    return
+  }
+  const isvalid = await bcrypt.compare(password,user.password)
+  if(!isvalid){
+    res.send("wrong password")
+    return
+  }
+  //console.log(user)
+  res.send(user)
+})
+
+app.post('/register',async(req,res)=>{
+  const{newusername,newpassword,newname,newemail}=req.body
+  const hash = await bcrypt.hash(newpassword,10)
+  let matched = dbuser.find(Element=>
+    Element.username == newusername
+)
+if(matched){
+    res.send("username was used")
+}else{
+dbuser.push({
+    username: newusername,
+    password: hash,
+    name: newname,
+    email: newemail
+})
+console.log(dbuser)
+  res.send("registered")
+}
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- server.js
+++ server.js
@@ -1,66 +1,119 @@
-const bcrypt = require('bcrypt');
 const express = require('express')
 const app = express()
 const port = 3000
 
 let dbuser =[
+  {
+      username : "soo",
+      password: "password",
+      name: "Soo",
+      email: "soo@utem.edu.my "
+  },
+
+  {
+      username: "ali",
+      password: "123456",
+      name: "ali",
+      email: "ali@utem.edu.my "
+  },
+
+  {
+      username: "wee",
+      password: "1234567",
+      name: "wee",
+      email: "wee@utem.edu.my "
+  },
+
+  {
+      username: "hee",
+      password: "hee123",
+      name: "hee",
+      email: "hee@utem.edu.my "
+  },
+
+  {
+      username: "utah",
+      password: "gdyyrgg",
+      name: "utah",
+      email: "uuuu@utem.edu.my "
+  },
 ]
 app.use(express.json())
 
-
-app.post('/', (req, res) => {
-  let data= req.body
-  let loginresult = login(
-    data.username,
-    data.password
-  )
-  res.send({
-    status: loginresult,
-    originaldata: data,
-    date: Date.now
-  });
-});
-
-app.post('/login',async(req,res) =>{
-  const{username, password}=req.body;
-
-  const user = dbuser.find(user => user.username === username);
-
-  if(!user){
-    res.send({error:"user not found"});
-    return
-  }
-  const isvalid = await bcrypt.compare(password,user.password)
-  if(!isvalid){
-    res.send("wrong password")
-    return
-  }
-  //console.log(user)
-  res.send(user)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-app.post('/register',async(req,res)=>{
-  const{newusername,newpassword,newname,newemail}=req.body
-  const hash = await bcrypt.hash(newpassword,10)
-  let matched = dbuser.find(Element=>
-    Element.username == newusername
-)
-if(matched){
-    res.send("username was used")
-}else{
-dbuser.push({
-    username: newusername,
-    password: hash,
-    name: newname,
-    email: newemail
-})
-console.log(dbuser)
-  res.send("registered")
-}
-});
 
+app.post('/login',(req,res) =>{
+    const{username, password}=req.body;
+  
+    const user = dbuser.find(user => user.username === username);
+  
+    if(!user){
+      res.send({error:"user not found"});
+      return
+    }
+    const userp = dbuser.find(userp => userp.password === password)
+    if(!userp){
+      res.send("wrong password")
+      return
+    }
+    //console.log(user)
+    res.send(user)
+  })
+
+app.post('/register',(req,res)=>{
+  let data=req.body
+  res.send(
+    register(
+      data.username,
+      data.password,
+      data.name,
+      data.email
+    )
+  );
+});
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function login(username,password){
+  console.log("someone try to login with",username,password)
+  let matched = dbuser.find(Element=>
+      Element.username == username
+  )
+  if(matched){
+      if(matched.password == password ) {
+          return matched
+      } else {
+          return "Password not matched"
+      }
+  } else {
+      return "username not found"
+  }
+}
+
+function register(newusername,newpassword,newname,newemail){
+  let matched = dbuser.find(Element=>
+      Element.username == newusername
+  )
+  if(matched){
+      return "username was used"
+  }else{
+
+
+  dbuser.push({
+      username: newusername,
+      password: newpassword,
+      name: newname,
+      email: newemail
+  })
+  console.log("account has been created")
+
+
+
+}
+}
